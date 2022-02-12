cd4ec079766dfe7554c4b3b516a88e8e50ab7ac4
Gonzalo Monguillot <gon_lab@hotmail.com>
2022-02-12 10:26:15 -0300

    modificación 12-2

--- Proyecto1/AppProy/urls.py
+++ Proyecto1/AppProy/urls.py
@@ -4,4 +4,6 @@ from AppProy.views import *
 
 urlpatterns = [ 
      path('corredorformulario/', corredor_formulario, name= "CorredorFormulario"),
+     path('carreraformulario/', carrera_formulario, name= "CarreraFormulario"),
+     path('indumentariaformulario/', indumentaria_formulario, name= "IndumentariaFormulario"),
 ]
--- Proyecto1/AppProy/views.py
+++ Proyecto1/AppProy/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 from AppProy.forms import CorredorFormulario, CarreraFormulario, IndumentariaFormulario
-from AppProy.models import Corredor
+from AppProy.models import Corredor, Carrera, Indumentaria
 
 # Create your views here.
 
@@ -23,5 +23,42 @@ def corredor_formulario(request):
             corredor.save()
     
     miFormulario = CorredorFormulario() #formulario vacio
-
     return render(request, 'AppProy/corredorformulario.html', {"miFormulario": miFormulario})
+
+
+
+def carrera_formulario(request):
+    if request.method == 'POST':
+        miFormulario2 = CarreraFormulario(request.POST) #Aquí me llega toda la info del html
+        print(miFormulario2)
+        
+        if miFormulario2.is_valid:     #Si pasa la validación de Django 
+            informacion2 = miFormulario2.data     #el nombre no importa
+            
+            r_tipo_carrera = informacion2['Tipo_de_Carrera']
+            r_superficie = informacion2['Superficie']
+        
+            carrera = Carrera(Tipo_de_Carrera=r_tipo_carrera, Superficie=r_superficie)
+            carrera.save()
+
+    miFormulario2 = CarreraFormulario() #formulario vacio
+    return render(request, 'AppProy/carreraformulario.html', {"miFormulario2": miFormulario2})
+
+
+def indumentaria_formulario(request):
+    if request.method == 'POST':
+        miFormulario3 = IndumentariaFormulario(request.POST) #Aquí me llega toda la info del html
+        print(miFormulario3)
+        
+        if miFormulario3.is_valid:     #Si pasa la validación de Django 
+            informacion3 = miFormulario3.data     #el nombre no importa
+            
+            r_talle_remera = informacion3['talle_remera']
+            r_talle_zapatillas = informacion3['talle_zapatillas']
+        
+            talles = Indumentaria(talle_remera=r_talle_remera, talle_zapatillas=r_talle_zapatillas)
+            talles.save()
+
+    miFormulario3 = IndumentariaFormulario() #formulario vacio
+    return render(request, 'AppProy/indumentariaformulario.html', {"miFormulario3": miFormulario3})
+    
